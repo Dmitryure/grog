53646f14a1cf18ceaf0ffe94ca697f523027ab2d
dima <dmitryure@gmail.com>
2020-08-07 16:52:25 +0300

    solution deletestate

--- src/components/05DeleteState/DeleteState.js
+++ src/components/05DeleteState/DeleteState.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 import Name from './components/Name';
 
 export default function DeleteState() {
-  const [state, setState] = useState(['Vasya', 'Petya', 'Ilya']);
+  const [state, setState] = useState([{ name: 'Vasya', id: 1 }, { name: 'Petya', id: 2 }, { name: 'Ilya', id: 3 }]);
   return (
     <>
       <ul>
-        {state.map((el) => <Name name={el} />)}
+        {state.map((el) => <Name key={el.id} setState={setState} name={el.name} id={el.id} />)}
       </ul>
     </>
   );
--- src/components/05DeleteState/components/Name.js
+++ src/components/05DeleteState/components/Name.js
@@ -1,9 +1,22 @@
 import React from 'react';
 
 function Name(props) {
-  const { name } = props;
+  const { name, id, setState } = props;
 
-  return <li>{name}</li>;
+  return (
+    <>
+      <li>{name}</li>
+      <button
+        onClick={() => {
+          setState((state) => state.filter((el) => id !== el.id));
+        }}
+        type="button"
+      >
+        Удалить
+
+      </button>
+    </>
+  );
 }
 
 export default Name;
