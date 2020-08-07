b6d192eaee2770a0b8389a70f26e9d3f8b01b215
dima <dmitryure@gmail.com>
2020-08-07 22:39:29 +0300

    new tests

---
+++ src/cypress/integration/05_delete-state.spec.js
@@ -0,0 +1,25 @@
+import { ROUTE_DELETE_STATE } from '../../constants';
+
+describe('delete-state', () => {
+  beforeEach(() => {
+    cy.visit(ROUTE_DELETE_STATE);
+  });
+  it('list is present', () => {
+    cy.get('ul').contains('Vasya');
+    cy.get('ul').contains('Petya');
+    cy.get('ul').contains('Ilya');
+  });
+  it('has 3 buttons', () => {
+    cy.get('button').should('have.length', 3);
+  });
+  it('clicking all buttons leaves no ul', () => {
+    cy.get('button').click({ multiple: true });
+    cy.get('li').should('not.exist');
+  });
+  it('clicking on name should delete the name', () => {
+    cy.get('button').eq(1).click();
+    cy.get('ul').contains('Petya').should('not.exist');
+    cy.get('button').eq(1).click();
+    cy.get('ul').contains('Ilya').should('not.exist');
+  });
+});
