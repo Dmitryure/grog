596ef3aeb2b533e65b6d812333fbd3849b8f5462
dima <dmitryure@gmail.com>
2020-08-07 22:55:52 +0300

    todo version

--- src/components/05DeleteState/DeleteState.js
+++ src/components/05DeleteState/DeleteState.js
@@ -5,11 +5,11 @@ import Name from './components/Name';
 // Важно, что бы порядок удаления был соблюден,
 // то есть кнопка рядом с Васей не удаляет Илью
 export default function DeleteState() {
-  const [state, setState] = useState([{ name: 'Vasya', id: 1 }, { name: 'Petya', id: 2 }, { name: 'Ilya', id: 3 }]);
+  const [state, setState] = useState(['Vasya', 'Petya', 'Ilya']);
   return (
     <>
       <ul>
-        {state.map((el) => <Name key={el.id} setState={setState} name={el.name} id={el.id} />)}
+        {state.map((el) => <Name name={el} />)}
       </ul>
     </>
   );
