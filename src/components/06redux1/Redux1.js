e1482dd24be99af8e8ae9ad621e7c8d5b943e667
dima <dmitryure@gmail.com>
2020-08-08 12:51:24 +0300

    redux

--- src/components/06redux1/Redux1.js
+++ src/components/06redux1/Redux1.js
@@ -1,17 +1,17 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { addName } from '../../redux/actions';
+import { useSelector } from 'react-redux';
 
 function Redux1() {
-  const dispatch = useDispatch();
   const names = useSelector((state) => state.names);
   const [input, setInput] = useState('');
 
   return (
     <>
       <input value={input} onChange={(e) => setInput(e.target.value)} />
-      <button type="button" onClick={() => dispatch(addName(input))}>Добавить</button>
+      <button type="button">Добавить</button>
       <ul>
+        {/* подумай, почему не надо писать через && и подумай,
+         в каком случае оператор бы понадобился */}
         {names.map((el) => <li>{el}</li>)}
       </ul>
     </>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,8 @@
 // Только добавь редьюсер
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { createStore } from 'redux';
-import { reducer } from './reducers';
 
-export const store = createStore(reducer, { names: [] }, composeWithDevTools());
+export const store = createStore((state) => state, { names: [] }, composeWithDevTools());
 
 if (window.Cypress) {
   window.store = store;
