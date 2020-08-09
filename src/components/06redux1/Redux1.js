d9001da055e2689e4974f7591635ad189e8b0510
dima <dmitryure@gmail.com>
2020-08-09 13:24:53 +0300

    tests fixed

--- src/components/06redux1/Redux1.js
+++ src/components/06redux1/Redux1.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addName } from '../../redux/actions';
 
+// Сделай так, чтобы имеющийся инпут обновлял стейт редакса и отражал в ul
+// в редаксе должен быть объект с полем names (в store он уже прописан) и туда
+// должны добавляться значения из input по нажатию кнопки
+
 function Redux1() {
   const dispatch = useDispatch();
   const names = useSelector((state) => state.names);
