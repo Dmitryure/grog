78c9d7f726f98e450a5be96e3af7a4ac8deea165
vnikonov63 <nikonov.va@yandex.ru>
2020-09-12 21:31:01 +0300

    Release 7 add: -worjed with the state to change the color. INitial made in a way, that two blocks can be of different color, but them realized the problem and wrote it in a better way

--- src/components/07ActiveState/ActiveState.js
+++ src/components/07ActiveState/ActiveState.js
@@ -1,16 +1,30 @@
-import React, { useState } from 'react';
-import { Name } from './components/Name';
+import React, { useState } from "react";
+import { Name } from "./components/Name";
 
 // Сделай так, чтобы по клику компонент Name окрашивался из красного цвета в зеленый
 // Чтобы не ошибиться, в компонент Name уже были подключены классы
 export const ActiveState = () => {
-  const [list, setList] = useState(['Vasya', 'Petya', 'Grisha']);
+  const [list, setList] = useState([
+    { name: "Vasya", color: false },
+    { name: "Petya", color: false },
+    { name: "Grisha", color: false },
+  ]);
+
+  function turn(name) {
+    setList(
+      list.map((element) => {
+        return element.name === name
+          ? { ...element, color: !element.color }
+          : { ...element, color: false };
+      })
+    );
+  }
 
   return (
     <>
       <ul>
         {list.map((el) => (
-          <Name name={el} />
+          <Name name={el.name} color={el.color} func={turn} />
         ))}
       </ul>
     </>
