eec300df3c9d7919caa0486688dc5f1a9999fb97
vnikonov63 <nikonov.va@yandex.ru>
2020-09-12 16:17:57 +0300

    Release 2 add: -the fetch to an api using the useEffect hook

--- src/components/02BasicFetch/BasicFetch.js
+++ src/components/02BasicFetch/BasicFetch.js
@@ -1,12 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { FETCH_URL_JSON_SERVER } from '../../constants';
+import React, { useState, useEffect } from "react";
+import { FETCH_URL_JSON_SERVER } from "../../constants";
 
 // Сделайте запрос в базу данных FETCH_URL_JSON_SERVER
 // Отрендерите каждый пост в листе в формате
 // "Автор: Вася, title: привет"
 
 function BasicFetch() {
-  return <div>hello</div>;
+  const [state, setState] = useState([]);
+  useEffect(async () => {
+    const result = await fetch(FETCH_URL_JSON_SERVER);
+    const finalResult = await result.json();
+    setState(finalResult);
+  }, []);
+  if (state.length) {
+    state.map((element) => {
+      console.log(element);
+    });
+  }
+  return (
+    <ul>
+      {state.length &&
+        state.map((element) => {
+          return (
+            <li>
+              Автор:{element.name}, title:{element.title}
+            </li>
+          );
+        })}
+    </ul>
+  );
 }
 
 export default BasicFetch;
