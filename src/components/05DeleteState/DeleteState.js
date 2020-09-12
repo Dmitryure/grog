e6671cf3d83cab6887676c8c464a09f717471b15
vnikonov63 <nikonov.va@yandex.ru>
2020-09-12 20:18:29 +0300

    Release 5 did the delete exercise passing a function to the Child element as prop

--- src/components/05DeleteState/DeleteState.js
+++ src/components/05DeleteState/DeleteState.js
@@ -1,15 +1,30 @@
-import React, { useState } from 'react';
-import Name from './components/Name';
+import React, { useState } from "react";
+import Name from "./components/Name";
 
 // Сделай так, чтобы кнопки рядом с именами удаляли эти имена.
 // Важно, что бы порядок удаления был соблюден,
 // то есть кнопка рядом с Васей не удаляет Илью
 export default function DeleteState() {
-  const [state, setState] = useState(['Vasya', 'Petya', 'Ilya', 'Vasya', 'Vasya']);
+  const [state, setState] = useState([
+    { name: "Vasya", id: 1 },
+    { name: "Petya", id: 2 },
+    { name: "Ilya", id: 3 },
+    { name: "Vasya", id: 4 },
+    { name: "Vasya", id: 5 },
+  ]);
+  let child = (id) => {
+    setState(
+      state.filter((element) => {
+        return element.id != id;
+      })
+    );
+  };
   return (
     <>
       <ul>
-        {state.map((el) => <Name name={el} />)}
+        {state.map((el) => (
+          <Name name={el.name} id={el.id} key={el.id} toChild={child} />
+        ))}
       </ul>
     </>
   );
